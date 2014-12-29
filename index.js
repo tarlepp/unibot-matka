da90b064a6016a7fce029b96ac93abcab87d14c8
tarlepp <tarmo.leppanen@kase.fi>
2014-12-29 22:47:04 +0200

    Refactored stuff.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var _ = require('lodash');
 var helpers = require('unibot-helpers');
 
 /**
- * Matka plugin for UniBot.
+ * Matka plugin for UniBot. This plugin uses Google Distance Matrix API to get actual data.
  *
  * @param   {Object} options Plugin options object, description below.
  *      db: {mongoose} the mongodb connection
@@ -21,18 +21,25 @@ module.exports = function init(options) {
     var config = options.config;
 
     /**
-     * Default plugin configuration. These can be override on your UniBot config.js file
+     * Default plugin configuration. These can be override on your UniBot config.js file. Please note that you need to
+     * specify 'apiKey' value to your config.js file.
      *
      * @type    {{
      *              apiKey: string,
      *              units: string,
      *              language: string
+     *              messages: {
+     *                  success: string
+     *              }
      *          }}
      */
     var pluginConfig = {
         "apiKey": "",
         "units": "metric",
-        "language": "fi-FI"
+        "language": "fi-FI",
+        "messages": {
+            "success": "${nick}: distance ${origin} - ${destination} is ${distance.rows[0].elements[0].distance.text} and travel time is about ${distance.rows[0].elements[0].duration.text}"
+        }
     };
 
     // Merge configuration for plugin
@@ -40,21 +47,29 @@ module.exports = function init(options) {
         pluginConfig = _.merge(pluginConfig, config.plugins.matka);
     }
 
+    // Actual plugin implementation
     return function plugin(channel) {
         /**
-         * Todo add description.
+         * Helper function to get distance data for specified locations.
          *
          * @param   {string}    origin
          * @param   {string}    destination
          * @param   {string}    from
          */
         function getDistance(origin, destination, from) {
-            var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin + 'destinations=' + destination + '&language=' + pluginConfig.language + '&units=' + pluginConfig.units + '&key=' + pluginConfig.apiKey;
+            var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin + '&destinations=' + destination + '&language=' + pluginConfig.language + '&units=' + pluginConfig.units + '&key=' + pluginConfig.apiKey;
 
-            helpers.download(url, function(data) {
-                try {
-                    var distanceData = JSON.parse(data);
+            // Fetch distance data from Google Distance Matrix API.
+            helpers.downloadSsl(url, function downloadSsl(data) {
+                var distanceData = JSON.parse(data);
 
+                if (distanceData.status !== 'OK' && distanceData.rows[0].elements[0].status !== 'OK') {
+                    if (distanceData.status !== 'OK') {
+                        throw distanceData.error_message || distanceData.status;
+                    } else {
+                        throw distanceData.rows[0].elements[0].status;
+                    }
+                } else {
                     var templateVars = {
                         nick: from,
                         origin: origin,
@@ -62,21 +77,22 @@ module.exports = function init(options) {
                         distance: distanceData
                     };
 
-                    var message = "${nick}: matka ${origin} - ${destination} on ${distance.rows[0].elements[0].distance.text}";
-
-                    channel.say(_.template(message, templateVars))
-                } catch (error) {
-                    channel.say('Oh noes, error: ' + error, from);
+                    channel.say(_.template(pluginConfig.messages.success, templateVars))
                 }
             });
         }
 
+        // Regex for this plugin
         return {
             '!matka (.+)\\|(.+)': function command(from, matches) {
-                if (_.empty(pluginConfig.apiKey)) {
+                if (_.isEmpty(pluginConfig.apiKey)) {
                     channel.say("You need to set Google Maps API key!", from);
                 } else {
-                    getDistance(matches[1], matches[2], from);
+                    try {
+                        getDistance(matches[1], matches[2], from);
+                    } catch (error) {
+                        channel.say('Oh noes, error: ' + error, from);
+                    }
                 }
             }
         };
